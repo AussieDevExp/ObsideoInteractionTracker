5ec16f7359dda92847ea213485739f96ee5dea14
Jayman Matthews <jaymanmatthews101@gmail.com>
2021-08-31 17:56:21 +0800

    v0.014

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data: function() {
       return {
-        current_app_version: "0.013",
+        current_app_version: "0.014",
         all_ghost_types: ["Effigy", "Rusalka", "Demon", "Shade", "Oni", "Yurei", "Mare", "Chimera"],
         all_interactions,
         current_selected_interactions: new Array()
@@ -47,6 +47,21 @@ const app = Vue.createApp({
         }
         if (string == "") string += "None"; 
         return string;
+      },
+      clearAllInteractions() {
+        /*let checkboxes = document.getElementsByClassName("interaction-checkbox");
+        for (let checkbox of checkboxes) {
+          checkbox.checked = false;
+          this.current_selected_interactions.splice(this.current_selected_interactions.indexOf(checkbox.value), 1)
+        }*/
+        let checkboxes = document.getElementsByClassName("interaction-checkbox");
+        //let checked_checkboxes = checkboxes.filter(val => val.checked);
+        let checked_checkboxes = Array.prototype.filter.call(checkboxes, val => val.checked);
+        for (let checkbox of checked_checkboxes) {
+          checkbox.checked = false;
+          this.current_selected_interactions.splice(this.current_selected_interactions.indexOf(checkbox.value), 1);
+        }
+        //this.current_selected_interactions = new Array();
       }
     },
     template: `
@@ -56,11 +71,12 @@ const app = Vue.createApp({
           <span style="font-weight:bold;">Possible Ghost Type/s:</span> <span>{{getPossibleGhostTypeDisplay()}}</span>
           <span style="font-weight:bold; float:right;">v{{current_app_version}}</span>
         </div>
+        <div class="can-be-clicked" @click="clearAllInteractions" style="display:inline-block; border:3px solid black; padding:2px;">Clear</div>
       </div>
       <div style="overflow:auto; height:95%; width:100%;">
         <div style="display:inline-flex; flex-wrap:wrap; flex-direction:column; max-height:100%;">
           <div v-for="(interaction, idx) in all_interactions.map(val => val[1])" style="flex:1; margin:2px; padding:2px; border: 2px solid black;">
-            <input class="can-be-clicked" type="checkbox" :id="'interaction'+idx" :value="interaction" v-model="current_selected_interactions">
+            <input class="interaction-checkbox can-be-clicked" type="checkbox" :id="'interaction'+idx" :value="interaction" v-model="current_selected_interactions">
             <label class="can-be-clicked" :for="'interaction'+idx">{{interaction}}</label>
           </div>
         </div>
@@ -70,3 +86,4 @@ const app = Vue.createApp({
 });
 
 // TODO: make whole div change checkbox... Is there even a way to do this? lol
+// TODO: add mechanic to show the items needed for the exorcism based on ghost type (e.g. crucific and holy water).
